45220039fc9f53d59b9e39ec939624013ba354b3
MHN715 <nielsen.177@gmail.com>
2020-06-25 22:17:01 +0200

    kinda finshed for desktop, at least for show

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,17 @@ document.addEventListener('DOMContentLoaded', function(){
     let links = document.querySelectorAll('.nav__list-item');
     let InYaFaceText = document.querySelectorAll('.index-main__heading3, .index-main__heading2, .index-main__call-to-action');
     let h1 = document.querySelector('.index-main__heading1');
-
+    
     let footer = document.querySelector('.footer');
     let footerInfoBox1Sect2 = document.querySelectorAll('.info-box1__sect2-heading, .info-box1__sect2-text1, .info-box1__sect2-text2, .info-box1__btn');
+    let footerInfoBox2Sect1 = document.querySelectorAll ('.info-box2__sect1-heading, .info-box2__sect1-text1, .info-box2__sect1-text2, .info-box2__sect1-link-section');
+    let footerSoMeLinks = document.querySelectorAll('.info-box2__sect1-link-section-links');
+    let footerInfoBox2MapPin = document.querySelector('.info-box2__map-pin-point');
 
     let backgroundBack = document.querySelector('.background-wrapper1');
     let backgroundFront = document.querySelector('.background-wrapper2');
     
-    function contentAnim(elm, duration, y, x, scale, opacityValue, dir){
+    function contentAnim(elm, duration, x, y, scale, opacityValue, dir){
         anime({
             targets: elm,
             translateY: y,
@@ -25,28 +28,19 @@ document.addEventListener('DOMContentLoaded', function(){
           });
     }
 
-    function backgroundAnim(elm, scale, x, y, dur){
-        anime({
-            targets: elm,
-            translateX: x,
-            translateY: y,
-            scale: 1.6,
-            direction: 'reverse',
-            easing: 'easeInOutQuad',
-            duration: dur,
-            // loop: true,
-          });
-    }
+    contentAnim(backgroundBack, 5000, -200, 100, 2, 1, 'reverse');
+    contentAnim(backgroundFront, 5000, 250, 220, 2, 1, 'reverse');
 
-    backgroundAnim(backgroundBack, 1.9, -300, 200, 25000);
-    backgroundAnim(backgroundFront, 1.9, 0, 200, 14000);
+    contentAnim(h1, 5500, 0, -100, 1, 0, 'reverse');
+    contentAnim(headerContent, 3000, 0, -100, 1, 0, 'reverse');
+    contentAnim(links, 2000, 0, -100, 1, 0, 'reverse');
+    contentAnim(InYaFaceText,4500, 0, 100, 1, 0, 'reverse');
+    
+    contentAnim(footer, 3000, 0, 100, 1, 0, 'reverse');
+    contentAnim(footerInfoBox1Sect2, 3500, -10, 100, 1, 0, 'reverse');
+    contentAnim(footerInfoBox2Sect1,  3500, 0, 100, 1, 0, 'reverse');
+    contentAnim(footerSoMeLinks, 3500, -10, 100, 1, 0, 'reverse');
+    contentAnim(footerInfoBox2MapPin, 3500, 300, 0, 1, 0, 'reverse')
+    
+});
 
-    contentAnim(h1, 25000, -100, 0, 1, 0, 'reverse');
-    contentAnim(headerContent, 3000, -100, 0, 1, 0, 'reverse');
-    contentAnim(links, 2000, -100, 0, 1, 0, 'reverse');
-    contentAnim(InYaFaceText,5000, 100, 0, 1, 0, 'reverse');
-    
-    contentAnim(footerInfoBox1Sect2, 3500, 100, -10, 1, 0, 'reverse')
-    contentAnim(footer, 3000, 100, 0, 1, 0, 'reverse');
-    
-})
